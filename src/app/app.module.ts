afab36462f40f9a9fe0aa7f1631acc1dbea3e639
Alexgg420 <alexgg888999@gmail.com>
2024-12-06 01:12:27 +0100

    puesta en producción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PartyModalComponent } from './components/party-modal/party-modal.compon
 import { PriceInputComponent } from './components/price-input/price-input.component';
 import { MapFrameComponent } from './components/map-frame/map-frame.component';
 import { SafeUrlPipe } from './components/pipes/safe-url.pipe';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,12 +44,12 @@ import { SafeUrlPipe } from './components/pipes/safe-url.pipe';
     { provide: BACKEND_TOKEN, useValue: 'strapi' },
     { provide: PEOPLE_RESOURCE_NAME_TOKEN, useValue: 'people' },
     { provide: PARTY_RESOURCE_NAME_TOKEN, useValue: 'parties' },
-    { provide: PEOPLE_API_URL_TOKEN, useValue: 'http://localhost:1337/api' },
-    { provide: PARTY_API_URL_TOKEN, useValue: 'http://localhost:1337/api' },
-    { provide: AUTH_SIGN_IN_API_URL_TOKEN, useValue: 'http://localhost:1337/api/auth/local' },
-    { provide: AUTH_SIGN_UP_API_URL_TOKEN, useValue: 'http://localhost:1337/api/auth/local/register' },
-    { provide: AUTH_ME_API_URL_TOKEN, useValue: 'http://localhost:1337/api/users/me' },
-    { provide: UPLOAD_API_URL_TOKEN, useValue: 'http://localhost:1337/api/upload' },
+    { provide: PEOPLE_API_URL_TOKEN, useValue: '${environment.apiUrl}/api' },
+    { provide: PARTY_API_URL_TOKEN, useValue: '${environment.apiUrl}/api' },
+    { provide: AUTH_SIGN_IN_API_URL_TOKEN, useValue: '${environment.apiUrl}/api/auth/local' },
+    { provide: AUTH_SIGN_UP_API_URL_TOKEN, useValue: '${environment.apiUrl}/api/auth/local/register' },
+    { provide: AUTH_ME_API_URL_TOKEN, useValue: '${environment.apiUrl}/api/users/me' },
+    { provide: UPLOAD_API_URL_TOKEN, useValue: '${environment.apiUrl}/api/upload' },
 
     PeopleMappingFactory,
     PartyMappingFactory,
